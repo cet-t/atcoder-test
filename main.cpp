d85944564a34a9708a781184ca03b037ee7be767
cet <cets021@gmail.com>
2023-12-22 23:07:04 +0900

    first commit

---
+++ main.cpp
@@ -0,0 +1,29 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    char s[N + 1] = {};
+    for (int i = 0; i < N; ++i)
+    {
+        cin >> s[i];
+    }
+    s[N] = '\0';
+
+    bool isIncY = false;
+    for (int i = 0; i < N; ++i)
+    {
+        if (s[i] == 'Y')
+        {
+            isIncY = true;
+            continue;
+        }
+    }
+    if (isIncY)
+        cout << "Four" << endl;
+    else
+        cout << "Three" << endl;
+}
